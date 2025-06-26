7ebd9bf31176cb9df17817c64ef04d02b5bc9c39
Michael Munga <micknyalle@gmail.com>
2025-06-26 13:43:14 +0300

    Add helper to get current user from JWT

--- phase-4/employee-tracker-backend/resources/reviews.py
+++ phase-4/employee-tracker-backend/resources/reviews.py
@@ -3,3 +3,7 @@ from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models import db, Employee, PerformanceReview
 from datetime import datetime
+
+
+def current_user():
+    return Employee.query.get(get_jwt_identity())
