3d8607ef0f35cfa757f74370ac756acbcc42ae1a
Michael Munga <micknyalle@gmail.com>
2025-06-26 13:44:18 +0300

    Add ReviewListResource with GET method for managers and employees

--- phase-4/employee-tracker-backend/resources/reviews.py
+++ phase-4/employee-tracker-backend/resources/reviews.py
@@ -7,3 +7,17 @@ from datetime import datetime
 
 def current_user():
     return Employee.query.get(get_jwt_identity())
+
+
+class ReviewListResource(Resource):
+    @jwt_required()
+    def get(self):
+        user = current_user()
+        if user.role_name == "Manager":
+            reviews = PerformanceReview.query.join(Employee).filter(
+                Employee.department_id == user.department_id
+            ).all()
+        else:
+            reviews = PerformanceReview.query.filter_by(employee_id=user.id).all()
+
+        return make_response([r.to_dict() for r in reviews], 200)
