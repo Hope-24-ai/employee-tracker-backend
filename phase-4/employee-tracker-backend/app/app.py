d1013bf6693e03be5ea4c717e775bce040d3ce1d
Michael Munga <micknyalle@gmail.com>
2025-06-28 16:52:46 +0300

    Initialize Flask app with environment config, database, JWT, and extensions

---
+++ phase-4/employee-tracker-backend/app/app.py
@@ -0,0 +1,47 @@
+import os
+from datetime import timedelta
+from dotenv import load_dotenv
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from models import db
+
+
+
+
+# Load environment variables 
+load_dotenv()
+
+# Initialize Flask App 
+app = Flask(__name__)
+
+# Configurations 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI") 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET")
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=24)
+
+ 
+
+#  Extensions Initialization 
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+migrate = Migrate(app, db)
+db.init_app(app)
+CORS(app)
+api = Api(app)
+
+
+
+
+
+
+
+
+# Entry Point 
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
--- phase-4/employee-tracker-frontend/src/pages/Departments.jsx
+++
@@ -1,8 +0,0 @@
-export default function Departments() {
-  return (
-    <div className="p-6">
-      <h2 className="text-xl font-bold">Departments Page</h2>
-      {/* Your content here */}
-    </div>
-  );
-}
